d3e010f43d8c8d1f02c7b646ab4fdf039619e2f5
Andre Odendaal <aodendaal.direct@gmail.com>
2017-06-11 17:55:42 -0500

    first github commit

--- blue_yellow_app/views.py
+++ blue_yellow_app/views.py
@@ -1,6 +1,24 @@
 from pyramid.view import view_config
+import blue_yellow_app.utils
 
 
-@view_config(route_name='home', renderer="templates/mytemplate.pt")
-def my_view(request):
-    return {'project': 'blue_yellow_app'}
+@view_config(route_name='home', renderer="templates/index.pt")
+def index(_):
+    return extend_model({})
+
+
+@view_config(route_name='box', renderer='templates/box_model.pt')
+def box_model(_):
+    return extend_model({})
+
+@view_config(route_name='layout', renderer='templates/layout.pt')
+def layout(_):
+    return extend_model({})
+
+
+def extend_model(model_dict):
+    model_dict['build_cache_id'] = blue_yellow_app.utils.build_cache_id
+    return model_dict
+
+
+
